cb5a3f066fa4c8fcdf189db0fd2db901a7deb7fe
Daiane Mascarenhas <adm.daianesilva@gmail.com>
2021-04-29 18:37:14 -0300

    feat: preset-env

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -9,7 +9,12 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(scss|css)$/,
